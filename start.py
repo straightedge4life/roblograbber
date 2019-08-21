5ba6dd275f9509a385d1666145d690a4b2613681
Rob Lee <robstraightedge4life@gmail.com>
2019-08-21 14:35:22 +0800

    优化代码

--- start.py
+++ start.py
@@ -9,72 +9,46 @@ import chardet
 
 
 def start():
+
     url = 'http://www.roblog.top'
-    #一些xpath规则
     articles_rule = '//div[@id="main"]/article'
     article_url_rule = './h2/a/@href'
     pageinate_rule = '//ol[@class="page-navigator"]/li[last()-1]/a/text()'
-    title_rule = '/html/body/div/div/div/div[1]/article/h1/a/text()'
+    articles_urls = []
+    insertData = []
 
     #先把首页挖出来
     paginate = getPage(url , pageinate_rule)
-    pages = range(1, int(paginate[0])+1)
+    
+    #根据分页信息获取最大页码，遍历拼接每一页的链接
+    pages = [ {'url':url+'/page/'+str(i) , 'rule':articles_rule} for i in range(1, int(paginate[0])+1)]
+    
+    #开线程抓取每页的内容，抓取每页的文章链接
+    articles_futures = threadCreate(10 , getPage , *pages)
+    for items in articles_futures :
+        for item in items.result() :
+            url = locateHTML(item , article_url_rule)[0]
+            articles_urls.append({'url':url})
+    
+    #再开线程抓取每篇文章的内容
+    detail_futures = threadCreate(10 , getArticlesPage , *articles_urls)
+    for detail in detail_futures:
+        insertData.append(detail.result())
 
-    #开个list保存每个进程的future对象
-    articles_futures = []
-    with ThreadPoolExecutor(max_workers = 10) as index_processer:
-        for i in pages :
-            p = index_processer.submit(getPage , url = url+'/page/'+str(i) , rule = articles_rule)
-            articles_futures.append(p)
-
-    #在全部进程完成后再从list通过.result()抽取函数所返回的数据
-    articles_urls = []
-    for article_future in articles_futures :
-        for article in article_future.result() :
-            articles_urls.append(article.xpath(article_url_rule)[0])
-           
-        
-    with ThreadPoolExecutor(max_workers = 10) as article_processer:
-        detail_futures = []
-        for url in articles_urls:
-            #抓文章标题
-            future = article_processer.submit(getArticlesPage , url = url )
-            detail_futures.append(future)
-
-        insertData = []
-        for detail in detail_futures :
-            insertData.append(detail.result())
-            
-
-
-    #写入到文件
-    # file_path = r'%s/%s.txt' % (os.path.dirname(os.path.abspath(__file__)), 'title')        
-    # for title_future in title_futures :
-    #     with  open(file_path , 'a+' , encoding = 'utf-8') as f :
-            
-    #         f.write(str(title_future.result()[0])+'\n')
-
+    #批量入库 这里需要优化 应该在上一步就入库
     client = mysql().client
     c = client.cursor()
     c.executemany("""INSERT INTO `articles` (`title`,`content`,`url`) VALUES(%s,%s,%s);""",insertData)
     client.commit()
-       
-    
-    
-            
-        
-    
 
+#专门用来抓取单个文章页
 def getArticlesPage(url):
-    # 格式
-    # [('title' , 'url' , 'content'),('title' , 'url' , 'content'),('title' , 'url' , 'content')]
-    #
     data = []
     title_rule = '/html/body/div/div/div/div[1]/article/h1/a/text()'
     content_rule = '//*[@id="main"]'
     g = grabber()
     page = etree.HTML(g.sendRquest(url).text)
-    
+
     title = page.xpath(title_rule)
 
     if title == () :
@@ -94,11 +68,34 @@ def getArticlesPage(url):
     data.append(url)
     return tuple(data)
 
+
+#发起请求 返回html对象
 def getPage(url , rule):
     g = grabber()
-    return etree.HTML(g.sendRquest(url).text).xpath(rule)
+    html = getHTML(g.sendRquest(url).text)
+    targetHtml = locateHTML(html , rule)
+    return targetHtml
+
+#解析html
+def getHTML(response):
+    return etree.HTML(response)
+
+#使用xpath定位
+def locateHTML(html , rule):
+    return html.xpath(rule)
     
+#开线程处理
+def threadCreate(max_workers , fn , *args):
+    futures = []
+    with ThreadPoolExecutor(max_workers) as pool:
+         for item in args :
+            future = pool.submit(fn , **item)
+            futures.append(future)
+    pool.shutdown()
+    return futures
+
 start()
+
 
 
 
